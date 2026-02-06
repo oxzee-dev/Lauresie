9db7574d94c84175242fff745292d1b0b3c2e59a
oxzee-dev <oxzeedev@gmail.com>
2026-02-06 21:10:53 +0100

    Update theme images in blogImages.ts
    Commented out an existing image URL and added a new one.

--- data/blogImages.ts
+++ data/blogImages.ts
@@ -1,6 +1,7 @@
 
 export const THEME_IMAGES = [
-  "https://images.unsplash.com/photo-1542332213-31f87348057f?auto=format&fit=crop&q=80&w=800", // Japanese Brush - Enso Circle/Abstract
+  //"https://images.unsplash.com/photo-1542332213-31f87348057f?auto=format&fit=crop&q=80&w=800", // Japanese Brush - Enso Circle/Abstract
+  "https://cdn.pixabay.com/photo/2015/11/07/11/24/trees-1031105_1280.jpg?auto=format&fit=crop&q=80&w=800", // Japanese Brush - Enso Circle/Abstract
   "https://images.unsplash.com/photo-1578301978018-3005759f48f7?auto=format&fit=crop&q=80&w=800", // Japanese Brush - Bamboo/Branch
   "https://images.unsplash.com/photo-1601208443914-3b2c4ca47f2b?auto=format&fit=crop&q=80&w=800", // Japanese Brush - Minimalist Mountain
   "https://images.unsplash.com/photo-1528459801416-a7e99a0dce3a?auto=format&fit=crop&q=80&w=800", // Japanese Brush - Flowing Ink
